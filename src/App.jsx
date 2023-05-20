30ad4db13829d0c680f8cb3cf13c46bd89bfe10a
Tony Zhang <zhgtony.pro@hotmail.com>
2023-05-20 17:48:22 +0200

    :lipstick: tablet & desktop responsive

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,8 @@ const App = () => {
   const [popularCities, setPopularCities] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
   const [popularFrom, setPopularFrom] = useState([]);
-  const [bgColor, setBgColor] = useState('#DCDFE9');
-  const [justify, setJustify] = useState('start');
-  
+  const [isActivated, setIsActivated] = useState(false);
+
   // Déclaration de fonctions
   // Fonctions asynchrone qui récupère des données avec axios en effectuant une requête GET vers l'url spécifiée et update l'état correspondant dans la fonction de mise à jour d'état
   const fetchPopularCities = async () => {
@@ -68,119 +67,135 @@ const App = () => {
     setPopularFrom([]);
   };
 
-  // Si bgColor est égal à #DCDFE9, alors on update bgColor à #5E90CC et justify à end, sinon on update bgColor à #DCDFE9 et justify à start
+  // Si isActivated est false alors bgColor est égal à #DCDFE9 et que justify est start, si isActivated est true alors on update bgColor à #5E90CC et justify à end
   const handleClick = () => {
-    (bgColor === '#DCDFE9') ? (setBgColor('#5E90CC'), setJustify('end')) : (setBgColor('#DCDFE9'), setJustify('start'));
+    setIsActivated(!isActivated);
   };
 
   return (
-    <div className="w-[100vw] h-[100vh] bg-[#F1F2F6] p-4">
-      <section className="flex flex-col items-start min-w-[150px] bg-white mt-[260px] rounded-[8px]">
-        {/* Menu section */}
+    <div className="flex justify-center p-4 ">
+      <section className="flex flex-col items-start min-w-[150px] bg-white mt-[260px] rounded-[8px]
+        tablet:max-w-[1136px]"
+      >
         <div className="flex flex-col gap-1 p-5 pb-0 w-full">
+          {/* Menu section */}
           <div className="flex justify-start gap-[22px] mb-1">
             <div className="flex items-center gap-[10px]">
-              <h3 className="text-[#132968] text-[14px]">One-way</h3>
-              <Icon icon="material-symbols:keyboard-arrow-down-rounded" width="24" height="24" />
+              <h3 className="text-[#132968] text-[14px] font-medium">One-way</h3>
+              <Icon icon="material-symbols:keyboard-arrow-down-rounded" width="24" height="24" color='#717FA4' />
             </div>
             <div className="flex items-center gap-[10px]">
-              <h3 className="text-[#132968] text-[14px]">1 adult, No discount card</h3>
-              <Icon icon="material-symbols:keyboard-arrow-down-rounded" width="24" height="24" />
+              <h3 className="text-[#132968] text-[14px font-medium">1 adult, No discount card</h3>
+              <Icon icon="material-symbols:keyboard-arrow-down-rounded" width="24" height="24" color='#717FA4' />
             </div>
           </div>
-        {/* End Menu section */}
-        {/* Search section */}
-          <div className="flex flex-col gap-1 relative">
-            <div className="flex items-center bg-[#F1F2F6] h-12 min-w-[150px] w-full gap-[10px] pl-2 rounded-[8px] hover:border-[1px] hover:border-[#a1a9c3] hover:border-solid">
-              <Icon icon="material-symbols:adjust-outline" width="24" height="24" color="#A1A9C3"/>
-              <input
-                type="text"
-                placeholder="From: City, Station Or Airport"
-                className="w-full bg-[#F1F2F6] outline-none"
-                onChange={handleInputChange}
-                onBlur={handleInputBlur}
-              />
+          {/* End Menu section */}
+        <div className="flex flex-col gap-1 tablet:flex-row tablet:gap-4">
+          {/* Search section */}
+            <div className="flex flex-col gap-1 relative">
+              <div className="flex items-center bg-[#F1F2F6] h-12 min-w-[150px] w-full gap-[10px] pl-2 pr-4 rounded-[8px] hover:border-[1px] hover:border-[#a1a9c3] hover:border-solid">
+                <Icon icon="material-symbols:adjust-outline" width="24" height="24" color="#A1A9C3"/>
+                <input
+                  type="text"
+                  placeholder="From: City, Station Or Airport"
+                  className="w-full bg-[#F1F2F6] outline-none"
+                  onChange={handleInputChange}
+                  onBlur={handleInputBlur}
+                />
+              </div>
+              <div className="w-full tablet:w-[424px] absolute top-12 bg-white rounded-[8px] shadow-lg z-10">
+                {searchResults.length > 0 && (
+                  <div>
+                    <h2 className="flex items-center h-full py-2 px-5">Résultats de la recherche :</h2>
+                    <ul>
+                      {searchResults.map((result, i) => (
+                        <li key={i} 
+                        onClick={() => handleCityFrom(result.local_name)} 
+                        className="flex items-center hover:bg-[#F1F2F6] px-5 py-4 gap-[10px] text-[#132968]">
+                          <div>
+                            <Icon icon="material-symbols:location-on-rounded" width="24" height="24"/>
+                          </div>
+                          <span className="text-sm tablet:text-base">
+                            {result.local_name}
+                          </span>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </div>
             </div>
-            <div className="w-full absolute top-12 bg-white rounded-[8px] shadow-lg z-10">
-              {searchResults.length > 0 && (
-                <div>
-                  <h2 className="flex items-center h-10 px-5">Résultats de la recherche :</h2>
+          {/* End Search section */}
+          {/* Popular section */}
+            <div className="flex flex-col gap-1 relative">
+              <div className="flex items-center bg-[#F1F2F6] h-12 min-w-[150px] gap-[10px] pl-2 pr-4 rounded-[8px] hover:border-[1px] hover:border-[#a1a9c3] hover:border-solid">
+                <Icon icon="material-symbols:location-on-rounded" width="24" height="24" color="#A1A9C3"/>
+                <input
+                  type="text"
+                  placeholder="To: City, Station Or Airport"
+                  className="w-full bg-[#F1F2F6] outline-none"
+                  onClick={() => handleCityFrom(searchInput)}
+                  onBlur={handleInputBlur}
+                />
+              </div>
+              <div className="w-full tablet:w-[424px] absolute top-12 bg-white rounded-[8px] shadow-lg z-10">
+                {popularFrom.length > 0 && (
+                  <div>
+                    <h2 className="flex items-center min-h-10 h-full py-2 px-5">Villes populaires au départ de {searchInput} :</h2>
+                    <ul>
+                      {popularFrom.map((city, i) => (
+                        <li key={i} className="flex items-center hover:bg-[#F1F2F6] px-5 py-4 gap-[10px] text-[#132968]">
+                         <div>
+                           <Icon icon="material-symbols:location-on-rounded" width="24" height="24"/>
+                         </div>
+                         <span className="text-sm tablet:text-base">
+                          {city.local_name}
+                          </span>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </div>
+            </div>
+          {/* End Popular section */}
+          {/* Popular cities section */}
+            <div className="flex flex-col gap-1 relative">
+              <div className="flex items-center bg-[#F1F2F6] h-12 min-w-[150px] gap-[10px] pl-2 pr-4 rounded-[8px] hover:border-[1px] hover:border-[#a1a9c3] hover:border-solid">
+                <Icon icon="mdi:bat" width="24" height="24" color="#A1A9C3"/>
+                <input
+                  type="text"
+                  placeholder="Afficher les villes populaires"
+                  className="w-full bg-[#F1F2F6] outline-none"
+                  onClick={handlePopularClick}
+                  onBlur={handleInputBlur}
+                />
+              </div>
+              <div className="w-full tablet:w-[424px] absolute top-12 bg-white rounded-[8px] shadow-lg z-10">
+                {popularCities.length > 0 && (
                   <ul>
-                    {searchResults.map((result, i) => (
-                      <li key={i} 
-                      onClick={() => handleCityFrom(result.local_name)} 
-                      className="flex items-center hover:bg-[#F1F2F6] px-5 h-12 gap-[10px] text-[#132968]">
-                        <Icon icon="material-symbols:location-on-rounded" width="24" height="24"/>
-                        {result.local_name}
-                      </li>
+                    {popularCities.map((city, i) => (
+                      <li key={i} className="flex items-center hover:bg-[#F1F2F6] px-5 py-4 text-[#132968] text-sm tablet:text-base">{city.local_name}</li>
                     ))}
                   </ul>
-                </div>
-              )}
+                )}
+              </div>
+            </div>
+          {/* End Popular cities section */}
+          {/* Search section */}
+            <div className="flex justify-center items-center bg-[#FA6B6B] h-12 min-w-[150px] gap-[10px] rounded-[8px] cursor-pointer hover:opacity-[80%] transition duration-200 ease-linear
+              w-full tablet:min-w-[70px] tablet:max-w-[185px]"
+            >
+              <span className="text-[15px] text-white">Search</span>
             </div>
           </div>
-        {/* End Search section */}
-        {/* Popular section */}
-          <div className="flex flex-col gap-1 relative">
-            <div className="flex items-center bg-[#F1F2F6] h-12 min-w-[150px] gap-[10px] pl-2 rounded-[8px] hover:border-[1px] hover:border-[#a1a9c3] hover:border-solid">
-              <Icon icon="material-symbols:location-on-rounded" width="24" height="24" color="#A1A9C3"/>
-              <input
-                type="text"
-                placeholder="To: City, Station Or Airport"
-                className="w-full bg-[#F1F2F6] outline-none"
-                onClick={() => handleCityFrom(searchInput)}
-                onBlur={handleInputBlur}
-              />
-            </div>
-            <div className="w-full absolute top-12 bg-white rounded-[8px] shadow-lg z-10">
-              {popularFrom.length > 0 && (
-                <div>
-                  <h2 className="flex items-center h-10 px-5">Villes populaires au départ de {searchInput} :</h2>
-                  <ul>
-                    {popularFrom.map((city, i) => (
-                      <li key={i} className="flex items-center hover:bg-[#F1F2F6] px-5 h-12 gap-[10px] text-[#132968]">
-                        <Icon icon="material-symbols:location-on-rounded" width="24" height="24"/>
-                        {city.local_name}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
-            </div>
-          </div>
-        {/* End Popular section */}
-        {/* Popular cities section */}
-          <div className="flex flex-col gap-1 relative">
-            <div className="flex items-center bg-[#F1F2F6] h-12 min-w-[150px] gap-[10px] pl-2 rounded-[8px] hover:border-[1px] hover:border-[#a1a9c3] hover:border-solid">
-              <Icon icon="mdi:bat" width="24" height="24" color="#A1A9C3"/>
-              <input
-                type="text"
-                placeholder="Afficher les villes populaires"
-                className="w-full bg-[#F1F2F6] outline-none"
-                onClick={handlePopularClick}
-                onBlur={handleInputBlur}
-              />
-            </div>
-            <div className="w-full absolute top-12 bg-white rounded-[8px] shadow-lg z-10">
-              {popularCities.length > 0 && (
-                <ul>
-                  {popularCities.map((city, i) => (
-                    <li key={i} className="flex items-center hover:bg-[#F1F2F6] px-5 h-12 text-[#132968]">{city.local_name}</li>
-                  ))}
-                </ul>
-              )}
-            </div>
-          </div>
-        {/* End Popular cities section */}
-        {/* Search section */}
-          <div className="flex justify-center items-center bg-[#FA6B6B] h-12 min-w-[150px] gap-[10px] pl-2 rounded-[8px] hover:opacity-[80%] transition duration-200 ease-linear">
-            <span className="text-[15px] text-white">Search</span>
-          </div>
+          {/* End Search section */}
         </div>
-        {/* End Search section */}
         {/* Accommodation section */}
         <div className="flex items-center p-4 gap-2">
-          <div onClick={handleClick} className={`flex justify-${justify} items-center w-9 h-5 bg-[${bgColor}] rounded-[30px] p-[2px] transition-all`}> <div className="bg-white rounded-full w-4 h-4"/> </div>
+        <div onClick={handleClick} className={`flex ${isActivated ? 'justify-end' : 'justify-start'} ${isActivated ? 'bg-[#5E90CC]' : 'bg-[#DCDFE9]'} items-center w-9 h-5 rounded-[30px] p-[2px] transition-all`}> 
+          <div className="bg-white rounded-full w-4 h-4"/> 
+        </div>
           <div>Find my accommodation</div>
         </div>
         {/* End Accommodation section */}
